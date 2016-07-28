7c1ec9f6fd6161fec5d24640337179e45b4f15c0
Shenglong Li <shenglong.li@outlook.com>
2016-07-28 16:33:57 -0700

    Editor showing content

--- src/app/+editor/editor.component.ts
+++ src/app/+editor/editor.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { TemplateManager } from '../template-manager';
+
 @Component({
   moduleId: __moduleName,
   selector: 'editor',
@@ -8,9 +10,26 @@ import { Router } from '@angular/router';
   styleUrls: ['editor.component.css'],
 })
 export class EditorComponent {
+  private content: string = '';
+  private subscription: any;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.router.navigateByUrl('/editor(sidebar:explorer)');
+
+    this.subscription = TemplateManager.emitter.subscribe((data) => {
+      this.refreshContent();
+    });
+
+    this.refreshContent();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private refreshContent() {
+    this.content = TemplateManager.template.toString();
   }
 }
