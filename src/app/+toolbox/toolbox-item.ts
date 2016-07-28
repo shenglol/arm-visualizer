969d931da98ad163cb0a26b1dcb2c5f2423c9554
Jason Young <jason@ytechie.com>
2016-07-28 16:50:53 -0700

    Added toolbox groups and items back

--- src/app/+toolbox/toolbox-item.ts
+++ src/app/+toolbox/toolbox-item.ts
@@ -4,10 +4,13 @@ export class ToolboxItem implements TreeViewNodeData {
   name: string;
   iconUrl: string;
   resource: string;
+  categoryName?: string;
 
-  constructor(iconUrl: string, name: string, resource: string, foo?: boolean) {
+  constructor(iconUrl: string, name: string, resource: string,
+    private hasDefaultJson?: boolean, categoryName?: string) {
     this.name = name;
     this.iconUrl = iconUrl;
     this.resource = resource;
+    this.categoryName = categoryName;
   }
 }
