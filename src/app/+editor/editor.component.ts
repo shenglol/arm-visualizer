9bc1f3beb56a41c32cf107fc715f4201089562b4
Shenglong Li <shenglong.li@outlook.com>
2016-07-29 01:54:35 -0700

    Monaco working

--- src/app/+editor/editor.component.ts
+++ src/app/+editor/editor.component.ts
@@ -1,35 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, ViewQuery } from '@angular/core';
+import { COMMON_DIRECTIVES  } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { TemplateManager } from '../template-manager';
+
+declare const monaco: any;
+declare const require: any;
 
 @Component({
   moduleId: __moduleName,
   selector: 'editor',
   templateUrl: 'editor.component.html',
   styleUrls: ['editor.component.css'],
+  directives: [COMMON_DIRECTIVES]
 })
 export class EditorComponent {
+  @ViewChild('editor') editorContent: ElementRef;
+
   private content: string = '';
   private subscription: any;
+  private editor: any;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.router.navigateByUrl('/editor(sidebar:explorer)');
-
     this.subscription = TemplateManager.emitter.subscribe((data) => {
       this.refreshContent();
     });
-
-    this.refreshContent();
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  private refreshContent() {
-    this.content = TemplateManager.template.toString();
+
+  ngAfterViewInit() {
+    var onGotAmdLoader = () => {
+      (<any>window).require(['vs/editor/editor.main'], () => {
+        this.initMonaco();
+        this.refreshContent();
+      });
+    };
+
+    if (!(<any>window).require) {
+      var loaderScript = document.createElement('script');
+      loaderScript.type = 'text/javascript';
+      loaderScript.src = 'vs/loader.js';
+      loaderScript.addEventListener('load', onGotAmdLoader);
+      document.body.appendChild(loaderScript);
+    } else {
+      onGotAmdLoader();
+    }
   }
+
+  initMonaco() {
+    let myDiv: HTMLDivElement = this.editorContent.nativeElement;
+    this.editor = monaco.editor.create(myDiv, {
+      value: '',
+      language: 'json'
+    });
+    console.log(1);
+  }
+
+   private refreshContent() {
+    this.editor.setValue(TemplateManager.template.toString());
+  }
+
 }
