066551389c8d941ffbb610809852558edcb2adfb
Shenglong Li <shenglong.li@outlook.com>
2016-08-02 11:04:01 -0700

    Update packages

--- jspm.config.js
+++ jspm.config.js
@@ -4,7 +4,7 @@ SystemJS.config({
       'babel-plugin-transform-es2015-typeof-symbol': 'npm:babel-plugin-transform-es2015-typeof-symbol@6.8.0',
       'babel-plugin-angular2-annotations': 'npm:babel-plugin-angular2-annotations@5.1.0',
       'babel-plugin-transform-flow-strip-types': 'npm:babel-plugin-transform-flow-strip-types@6.8.0',
-      'babel-plugin-transform-class-properties': 'npm:babel-plugin-transform-class-properties@6.10.2',
+      'babel-plugin-transform-class-properties': 'npm:babel-plugin-transform-class-properties@6.11.5',
       'babel-plugin-transform-decorators-legacy': 'npm:babel-plugin-transform-decorators-legacy@1.3.4',
       'path': 'github:jspm/nodelibs-path@0.2.0-alpha',
       'fs': 'github:jspm/nodelibs-fs@0.2.0-alpha',
@@ -21,33 +21,27 @@ SystemJS.config({
       },
       'npm:babel-plugin-transform-es2015-typeof-symbol@6.8.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2'
+          'babel-runtime': 'npm:babel-runtime@6.11.6'
         }
       },
       'npm:babel-plugin-transform-decorators-legacy@1.3.4': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
           'babel-template': 'npm:babel-template@6.9.0',
           'babel-plugin-syntax-decorators': 'npm:babel-plugin-syntax-decorators@6.8.0'
         }
       },
-      'npm:babel-plugin-transform-class-properties@6.10.2': {
-        'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
-          'babel-plugin-syntax-class-properties': 'npm:babel-plugin-syntax-class-properties@6.8.0'
-        }
-      },
       'npm:babel-plugin-transform-flow-strip-types@6.8.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
           'babel-plugin-syntax-flow': 'npm:babel-plugin-syntax-flow@6.8.0'
         }
       },
       'npm:babel-generator@6.11.4': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
           'source-map': 'npm:source-map@0.5.6',
-          'lodash': 'npm:lodash@4.13.1',
+          'lodash': 'npm:lodash@4.14.1',
           'detect-indent': 'npm:detect-indent@3.0.1',
           'babel-types': 'npm:babel-types@6.11.1',
           'babel-messages': 'npm:babel-messages@6.8.0'
@@ -56,38 +50,32 @@ SystemJS.config({
       'npm:babel-template@6.9.0': {
         'map': {
           'babel-types': 'npm:babel-types@6.11.1',
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
-          'lodash': 'npm:lodash@4.13.1',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
+          'lodash': 'npm:lodash@4.14.1',
           'babylon': 'npm:babylon@6.8.4',
-          'babel-traverse': 'npm:babel-traverse@6.11.4'
+          'babel-traverse': 'npm:babel-traverse@6.12.0'
         }
       },
       'npm:babel-plugin-syntax-decorators@6.8.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2'
-        }
-      },
-      'npm:babel-runtime@6.9.2': {
-        'map': {
-          'core-js': 'npm:core-js@2.4.1',
-          'regenerator-runtime': 'npm:regenerator-runtime@0.9.5'
+          'babel-runtime': 'npm:babel-runtime@6.11.6'
         }
       },
       'npm:babel-plugin-syntax-class-properties@6.8.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2'
+          'babel-runtime': 'npm:babel-runtime@6.11.6'
         }
       },
       'npm:babel-plugin-syntax-flow@6.8.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2'
+          'babel-runtime': 'npm:babel-runtime@6.11.6'
         }
       },
       'npm:babel-types@6.11.1': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
-          'babel-traverse': 'npm:babel-traverse@6.11.4',
-          'lodash': 'npm:lodash@4.13.1',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
+          'babel-traverse': 'npm:babel-traverse@6.12.0',
+          'lodash': 'npm:lodash@4.14.1',
           'to-fast-properties': 'npm:to-fast-properties@1.0.2',
           'esutils': 'npm:esutils@2.0.2'
         }
@@ -99,27 +87,14 @@ SystemJS.config({
           'get-stdin': 'npm:get-stdin@4.0.1'
         }
       },
-      'npm:babel-traverse@6.11.4': {
-        'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
-          'babel-types': 'npm:babel-types@6.11.1',
-          'babel-messages': 'npm:babel-messages@6.8.0',
-          'babylon': 'npm:babylon@6.8.4',
-          'lodash': 'npm:lodash@4.13.1',
-          'debug': 'npm:debug@2.2.0',
-          'babel-code-frame': 'npm:babel-code-frame@6.11.0',
-          'invariant': 'npm:invariant@2.2.1',
-          'globals': 'npm:globals@8.18.0'
-        }
-      },
       'npm:babylon@6.8.4': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2'
+          'babel-runtime': 'npm:babel-runtime@6.11.6'
         }
       },
       'npm:babel-messages@6.8.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2'
+          'babel-runtime': 'npm:babel-runtime@6.11.6'
         }
       },
       'npm:repeating@1.1.3': {
@@ -129,7 +104,7 @@ SystemJS.config({
       },
       'npm:babel-code-frame@6.11.0': {
         'map': {
-          'babel-runtime': 'npm:babel-runtime@6.9.2',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
           'esutils': 'npm:esutils@2.0.2',
           'chalk': 'npm:chalk@1.1.3',
           'js-tokens': 'npm:js-tokens@2.0.0'
@@ -195,6 +170,47 @@ SystemJS.config({
           'core-util-is': 'npm:core-util-is@1.0.2',
           'string_decoder': 'npm:string_decoder@0.10.31'
         }
+      },
+      'npm:babel-plugin-transform-class-properties@6.11.5': {
+        'map': {
+          'babel-helper-function-name': 'npm:babel-helper-function-name@6.8.0',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
+          'babel-plugin-syntax-class-properties': 'npm:babel-plugin-syntax-class-properties@6.8.0'
+        }
+      },
+      'npm:babel-helper-function-name@6.8.0': {
+        'map': {
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
+          'babel-traverse': 'npm:babel-traverse@6.12.0',
+          'babel-template': 'npm:babel-template@6.9.0',
+          'babel-helper-get-function-arity': 'npm:babel-helper-get-function-arity@6.8.0',
+          'babel-types': 'npm:babel-types@6.11.1'
+        }
+      },
+      'npm:babel-runtime@6.11.6': {
+        'map': {
+          'core-js': 'npm:core-js@2.4.1',
+          'regenerator-runtime': 'npm:regenerator-runtime@0.9.5'
+        }
+      },
+      'npm:babel-traverse@6.12.0': {
+        'map': {
+          'babel-code-frame': 'npm:babel-code-frame@6.11.0',
+          'globals': 'npm:globals@8.18.0',
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
+          'babel-messages': 'npm:babel-messages@6.8.0',
+          'babel-types': 'npm:babel-types@6.11.1',
+          'babylon': 'npm:babylon@6.8.4',
+          'invariant': 'npm:invariant@2.2.1',
+          'debug': 'npm:debug@2.2.0',
+          'lodash': 'npm:lodash@4.14.1'
+        }
+      },
+      'npm:babel-helper-get-function-arity@6.8.0': {
+        'map': {
+          'babel-runtime': 'npm:babel-runtime@6.11.6',
+          'babel-types': 'npm:babel-types@6.11.1'
+        }
       }
     }
   },
@@ -233,8 +249,8 @@ SystemJS.config({
     '@angular/platform-browser': 'npm:@angular/platform-browser@2.0.0-rc.4',
     '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@2.0.0-rc.4',
     '@angular/router': 'npm:@angular/router@3.0.0-beta.2',
-    '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap@1.0.0-alpha.0',
-    'arm-visualizer-engine': 'npm:arm-visualizer-engine@0.0.5',
+    '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap@1.0.0-alpha.1',
+    'arm-visualizer-engine': 'npm:arm-visualizer-engine@0.0.6',
     'buffer': 'github:jspm/nodelibs-buffer@0.2.0-alpha',
     'es6-promise': 'npm:es6-promise@3.2.1',
     'es6-shim': 'npm:es6-shim@0.35.1',
