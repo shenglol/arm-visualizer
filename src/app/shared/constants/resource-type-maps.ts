fe339845cd948e42c3cc599cad5a1ca0feff9f0c
Shenglong Li <shenglong.li@outlook.com>
2016-08-03 23:20:44 -0700

    Add LoadBalancer mapping

--- src/app/shared/constants/resource-type-maps.ts
+++ src/app/shared/constants/resource-type-maps.ts
@@ -1,15 +1,17 @@
 export const TYPE_DISPLAY_NAME_MAP = {
   'Microsoft.Web/sites': 'Website',
   'Microsoft.Compute/virtualMachines': 'Virtual Machine',
+  'Microsoft.Network/loadBalancers': 'Load Balancer',
   'Microsoft.Network/publicIPAddresses': 'Public IP Address',
-  'Microsoft.Network/virtualNetworks': 'Virtual Network',
+  'Microsoft.Network/virtualNetworks': 'Virtual Network'
 };
 
 export const TYPE_ICON_NAME_MAP = {
   'Microsoft.Web/sites': 'Website',
   'Microsoft.Compute/virtualMachines': 'VirtualMachine',
+  'Microsoft.Network/loadBalancers': 'LoadBalancer',
   'Microsoft.Network/publicIPAddresses': 'IPAddress',
-  'Microsoft.Network/virtualNetworks': 'VirtualNetwork',
+  'Microsoft.Network/virtualNetworks': 'VirtualNetwork'
 };
 
 export const DEFAULT_ICON_NAME = 'Gear';
